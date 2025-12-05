d9e2eeebc063935c1a2adab80f0301fde6f733bb
Andrew Null <andrew@nulls.us>
2025-12-04 20:04:32 -0700

    Data ready for insertion

--- database_setup/insert_data.sql
+++ database_setup/insert_data.sql
@@ -1,11 +1,11 @@
 ﻿/*
 Ctrl+F and Replace 
-WORKING_DIRECTORY
+CSVs
 with the directory that contains your .tbl files
 */
-COPY company FROM 'WORKING_DIRECTORY/company.tbl' WITH (delimiter '|', NULL '');
-COPY cellphonecompany FROM 'WORKING_DIRECTORY/cellphonecompany.tbl' WITH (delimiter '|', NULL '');
-COPY chipmanufacturer FROM 'WORKING_DIRECTORY/chipmanufacturer.tbl' WITH (delimiter '|', NULL '');
-COPY phonemodel FROM 'WORKING_DIRECTORY/phonemodel.tbl' WITH (delimiter '|', NULL '');
-COPY chip FROM 'WORKING_DIRECTORY/chip.tbl' WITH (delimiter '|', NULL '');
-COPY contains FROM 'WORKING_DIRECTORY/contains.tbl' WITH (delimiter '|', NULL '');
+COPY company FROM 'CSVs/company.csv' WITH (delimiter ',', NULL '');
+COPY cellphonecompany FROM 'CSVs/phonecomp.csv' WITH (delimiter ',', NULL '');
+COPY chipmanufacturer FROM 'CSVs/chipcomp.csv' WITH (delimiter ',', NULL '');
+COPY phonemodel FROM 'CSVs/phone.csv' WITH (delimiter ',', NULL '');
+COPY chip FROM 'CSVs/chip.csv' WITH (delimiter ',', NULL '');
+COPY contains FROM 'CSVs/contains.csv' WITH (delimiter ',', NULL '');
